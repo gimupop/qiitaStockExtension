025a618235b7017cf46afb20db4060201f3dd767
gimupop <gimutokenri@gmail.com>
2016-11-30 01:20:03 +0900

    いいねを完全にストックで上書き

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(function() {
   var sutock = $('<button class="p-button"><span class="StockButton__label">ストック</span></button>');
   sutock.on("click", function(){$($(".StockButton__stock")[0]).click()});
-  //sutock.attr("style","position:absolute;")
+  //この行をコメントアウトすれば、ストックといいねの分割表示
+  $(".ArticleMainHeader .LikeButton .p-button").attr("style","display:none");
   $(".ArticleMainHeader .LikeButton").append(sutock);
 });
