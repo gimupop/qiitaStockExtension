8d6dc435e0892114e022197d1f5146cdb3c840df
gimupop <gimutokenri@gmail.com>
2016-11-30 03:21:02 +0900

    書き換えたストックボタン押下時にいいねも発火するように変更

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # qiitaStockExtension
-chorome extension　for japanees website Qiita 
+chorome extension for japanees website Qiita
 http://qiita.com/
-
+Qiitaの拡張なので日本語で書く。
+<br>
 Qiitaのいいねボタンをストックボタンに変更するchoromeの拡張。
-
+<br>
+以下の手順で試せる
+本リポジトリをgit clone
+chrome://extensions/
+デベロッパーモードにチェック
+cloneしたディレクトリを読み込む読み込む
+<br>
+挙動
+画面上部のいいねボタンをストックに変更
+クリックするといいねとストック両方行う
 http://gimpop.hatenablog.com/entry/2016/11/30/001100
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(function() {
   var sutock = $('<button class="p-button"><span class="StockButton__label">ストック</span></button>');
   sutock.on("click", function(){$($(".StockButton__stock")[0]).click()});
+  sutock.on("click", function(){$($(".p-button")[0]).click()});
   //この行をコメントアウトすれば、ストックといいねの分割表示
   $(".ArticleMainHeader .LikeButton .p-button").attr("style","display:none");
   $(".ArticleMainHeader .LikeButton").append(sutock);
